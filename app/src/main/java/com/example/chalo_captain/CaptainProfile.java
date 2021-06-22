ce9db23758cda3d3058af74229d60a25521f2eea
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-23 03:48:32 +0500

    dashboard page added..

--- app/src/main/java/com/example/chalo_captain/CaptainProfile.java
+++ app/src/main/java/com/example/chalo_captain/CaptainProfile.java
@@ -2,13 +2,29 @@ package com.example.chalo_captain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CaptainProfile extends AppCompatActivity {
+
+    private Button updateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_captain_profile);
+        updateButton = (Button)findViewById(R.id.updateCaptain);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
+    }
+    public void openLogin(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/chalo_captain/Login.java
+++ app/src/main/java/com/example/chalo_captain/Login.java
@@ -7,9 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity{
 
     private Button signup_button;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +24,21 @@ public class Login extends AppCompatActivity {
                 openRegistration();
             }
         });
+
+        login_button = (Button)findViewById(R.id.login_Page);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginPage();
+            }
+        });
     }
     public void openRegistration(){
         Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
+    }
+    public void loginPage(){
+        Intent intent = new Intent(this, Dashboard.class);
         startActivity(intent);
     }
 }
