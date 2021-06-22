1279e55c8d1edf073928cf2566146ccb09ce1483
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-23 03:13:49 +0500

    pages interlinked..

--- app/src/main/java/com/example/chalo_captain/Login.java
+++ app/src/main/java/com/example/chalo_captain/Login.java
@@ -2,13 +2,30 @@ package com.example.chalo_captain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button signup_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signup_button = (Button)findViewById(R.id.signup_page);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistration();
+            }
+        });
+    }
+    public void openRegistration(){
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/chalo_captain/Register.java
+++ app/src/main/java/com/example/chalo_captain/Register.java
@@ -2,13 +2,42 @@ package com.example.chalo_captain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+    private Button signin_button;
+    private Button register_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signin_button = (Button)findViewById(R.id.login_page);
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginPage();
+            }
+        });
+        register_button = (Button)findViewById(R.id.register_page);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCaptainPage();
+            }
+        });
+    }
+    public void openLoginPage(){
+        Intent intent= new Intent(this, Login.class);
+        startActivity(intent);
+    }
+    public void openCaptainPage(){
+        Intent intent= new Intent(this, CaptainProfile.class);
+        startActivity(intent);
     }
 }
