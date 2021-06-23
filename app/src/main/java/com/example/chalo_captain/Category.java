37112357a75ba8f934db1b354bc1f820b214d6b0
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-24 03:37:40 +0500

    back button added to category activity

--- app/src/main/java/com/example/chalo_captain/Category.java
+++ app/src/main/java/com/example/chalo_captain/Category.java
@@ -3,13 +3,28 @@ package com.example.chalo_captain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Category extends AppCompatActivity {
 
+    //ImageView back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+//        back_button = back_button.findViewById(R.id.back_pressed);
+//
+//        back_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Category.super.onBackPressed();
+//            }
+//        });
     }
 }
