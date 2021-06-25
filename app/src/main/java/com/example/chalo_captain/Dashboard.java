479c5a88faf0746f89654d93aa3044383a52837f
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-25 16:29:56 +0500

    dashboard updated.. cards fixed..

--- app/src/main/java/com/example/chalo_captain/Dashboard.java
+++ app/src/main/java/com/example/chalo_captain/Dashboard.java
@@ -9,28 +9,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Dashboard extends AppCompatActivity {
-//    Toolbar toolbar;
-    private CardView homeButton;
+public class Dashboard extends AppCompatActivity implements View.OnClickListener{
+    private CardView D1,D2,D3,D4,D5,D6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        homeButton = (CardView)findViewById(R.id.homeCardView);
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openCategory();
-            }
-        });
-//        toolbar = findViewById(R.id.toolbar);
-//
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setTitle(null);
+        D1 = (CardView)findViewById(R.id.d1);
+        D2 = (CardView)findViewById(R.id.d2);
+        D3 = (CardView)findViewById(R.id.d3);
+        D4 = (CardView)findViewById(R.id.d4);
+        D5 = (CardView)findViewById(R.id.d5);
+        D6 = (CardView)findViewById(R.id.d6);
+
+        D1.setOnClickListener((View.OnClickListener)this);
+        D2.setOnClickListener((View.OnClickListener)this);
+        D3.setOnClickListener((View.OnClickListener)this);
+        D4.setOnClickListener((View.OnClickListener)this);
+        D5.setOnClickListener((View.OnClickListener)this);
+        D6.setOnClickListener((View.OnClickListener)this);
     }
-    public void openCategory(){
-        Intent intent = new Intent(this, Category.class);
-        startActivity(intent);
+    @Override
+    public void onClick(View v){
+        Intent intent;
+        switch (v.getId()){
+            case R.id.d1: intent = new Intent(this,Category.class); startActivity(intent); break;
+            case R.id.d2: intent = new Intent(this,Category.class); startActivity(intent); break;
+            case R.id.d3: intent = new Intent(this,Category.class); startActivity(intent); break;
+            case R.id.d4: intent = new Intent(this,Category.class); startActivity(intent); break;
+            case R.id.d5: intent = new Intent(this,Category.class); startActivity(intent); break;
+            case R.id.d6: intent = new Intent(this,Category.class); startActivity(intent); break;
+        }
     }
 }
