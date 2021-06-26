73d766fb7dac15676128b8d13de9caee61f190a8
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-27 02:41:26 +0500

    vehicle page updated..

--- app/src/main/java/com/example/chalo_captain/Booking.java
+++ app/src/main/java/com/example/chalo_captain/Booking.java
@@ -3,12 +3,38 @@ package com.example.chalo_captain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Booking extends AppCompatActivity {
+
+    ListView bookingListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        bookingListView = (ListView) findViewById(R.id.bookigList);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("abcd");
+        arrayList.add("aaaa");
+        arrayList.add("vvvv");
+        arrayList.add("ffff");
+        arrayList.add("ssss");
+        arrayList.add("ggggg");
+        arrayList.add("bbbb");
+        arrayList.add("kkkk");
+        arrayList.add("mmmm");
+        arrayList.add("oooo");
+        arrayList.add("pppp");
+        arrayList.add("kkkk");
+        arrayList.add("jjjj");
+        arrayList.add("uuuuu");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        bookingListView.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/chalo_captain/Vehicle.java
+++ app/src/main/java/com/example/chalo_captain/Vehicle.java
@@ -2,13 +2,31 @@ package com.example.chalo_captain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Vehicle extends AppCompatActivity {
+
+    private Button updateVehicle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle);
+
+        updateVehicle = (Button) findViewById(R.id.update_vehicle);
+        updateVehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCarDetails();
+            }
+        });
+    }
+
+    public void openCarDetails() {
+        Intent intent = new Intent(this, Captain_Update.class);
+        startActivity(intent);
     }
 }
