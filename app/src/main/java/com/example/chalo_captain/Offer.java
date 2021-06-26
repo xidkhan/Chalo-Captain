79ee4336840b7c1d167327d375f10e31ce0507e1
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-26 18:06:01 +0500

    offer page created..

--- app/src/main/java/com/example/chalo_captain/Offer.java
+++ app/src/main/java/com/example/chalo_captain/Offer.java
@@ -2,13 +2,31 @@ package com.example.chalo_captain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Offer extends AppCompatActivity {
+
+    private Button createOffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
+
+        createOffer = (Button)findViewById(R.id.createOfferButton);
+        createOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDashboard();
+            }
+        });
+    }
+
+    public void openDashboard(){
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
