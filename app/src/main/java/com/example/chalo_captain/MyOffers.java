b3ef9657f047b8397b75d6d957e4d179b58911c7
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-26 21:43:21 +0500

    request page updated..

--- app/src/main/java/com/example/chalo_captain/MyOffers.java
+++ app/src/main/java/com/example/chalo_captain/MyOffers.java
@@ -13,8 +13,6 @@ public class MyOffers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_offers);
-
-
     }
 
 }
