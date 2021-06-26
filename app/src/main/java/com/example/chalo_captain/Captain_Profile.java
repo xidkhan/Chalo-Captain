376e249958ebe1412c2e9a915f60e16ed6ce54c2
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-27 01:41:42 +0500

    captain profile page updated..

--- app/src/main/java/com/example/chalo_captain/Captain_Profile.java
+++ app/src/main/java/com/example/chalo_captain/Captain_Profile.java
@@ -2,13 +2,30 @@ package com.example.chalo_captain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Captain_Profile extends AppCompatActivity {
+
+    private Button captain_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_captain_profile);
+
+        captain_profile = (Button)findViewById(R.id.update_captain_profile);
+        captain_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCaptainUpdate();
+            }
+        });
+    }
+    public void openCaptainUpdate(){
+        Intent intent = new Intent(this, Captain_Update.class);
+        startActivity(intent);
     }
 }
