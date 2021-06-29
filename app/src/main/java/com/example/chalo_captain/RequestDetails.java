d4289468d911c772e9e47d88368109d6256ea41b
sadamccr <47504498+sadamccr@users.noreply.github.com>
2021-06-30 02:13:27 +0500

    recycler on requests updated

---
+++ app/src/main/java/com/example/chalo_captain/RequestDetails.java
@@ -0,0 +1,21 @@
+package com.example.chalo_captain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class RequestDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_details);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        Toast.makeText(this, "name" + name, Toast.LENGTH_SHORT).show();
+    }
+}
